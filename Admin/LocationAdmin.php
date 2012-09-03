c8965ccb51e362dec6302a92a1f48aa26b3ecd7b
Markus Poerschke <markus@eluceo.de>
2012-09-03 16:18:21 +0200

    Events can be edited inside of Location Admin

--- Admin/LocationAdmin.php
+++ Admin/LocationAdmin.php
@@ -5,6 +5,7 @@ namespace Eluceo\EventBundle\Admin;
 use Sonata\AdminBundle\Admin\Admin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
+use Knp\Menu\ItemInterface as MenuItemInterface;
 
 class LocationAdmin extends Admin
 {
@@ -24,5 +25,28 @@ class LocationAdmin extends Admin
             ->add('street')
             ->add('zip')
             ->add('city');
+    }
+
+    protected function configureSideMenu(MenuItemInterface $menu, $action, Admin $childAdmin = null)
+    {
+        $id = $this->getRequest()->get('id');
+
+        if (null !== $id) {
+            $menu->addChild(
+                'edit',
+                array(
+                    'label' => $this->trans('sidemenu_edit'),
+                    'uri'   => $this->generateUrl('edit', array('id' => $id))
+                )
+            );
+
+            $menu->addChild(
+                'projects',
+                array(
+                    'label'=> $this->trans('sidemenu_event_list'),
+                    'uri'  => $this->generateUrl('eluceo.event.admin.event.list', array('id' => $id))
+                )
+            );
+        }
     }
 }
