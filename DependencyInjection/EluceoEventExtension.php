0a7cc13e43500456d38a2cea9d1ca2f44d17e04e
Markus Poerschke <markus@eluceo.de>
2012-09-07 14:20:40 +0200

    Add EntityMangers as a service

--- DependencyInjection/EluceoEventExtension.php
+++ DependencyInjection/EluceoEventExtension.php
@@ -24,5 +24,8 @@ class EluceoEventExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+
+        // TODO: Detect ORM / ODM / etc...
+        $loader->load('orm.yml');
     }
 }
