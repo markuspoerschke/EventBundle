90c0b22059c12e111578326d1bd4f30d65c44459
Markus Poerschke <markus@eluceo.de>
2013-06-17 21:33:01 +0200

    Added __toString() method

--- Model/EventDate.php
+++ Model/EventDate.php
@@ -11,4 +11,10 @@ abstract class EventDate extends BaseEventDate
         $this->noTime = false;
         $this->active = false;
     }
+
+    function __toString()
+    {
+        $format = $this->noTime ? 'd.m.Y' : 'd.m.Y H:i';
+        return $this->startDatetime->format($format);
+    }
 }
