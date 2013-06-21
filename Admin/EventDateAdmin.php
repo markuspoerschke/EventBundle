d057ea09a2b0a5de1fc3887ad4beb7b11820e2d8
Markus Poerschke <markus@eluceo.de>
2013-06-21 18:38:03 +0200

    Added empty values for date

--- Admin/EventDateAdmin.php
+++ Admin/EventDateAdmin.php
@@ -10,10 +10,18 @@ class EventDateAdmin extends Admin
 {
     protected function configureFormFields(FormMapper $form)
     {
+        $emptyValue = array(
+            'year' => $this->trans('year'),
+            'month' => $this->trans('month'),
+            'day' => $this->trans('day'),
+            'hour' => $this->trans('hour'),
+            'minute' => $this->trans('minute')
+        );
+
         $form
             ->add('active', null, array('required' => false, 'value' => true))
-            ->add('startDatetime')
-            ->add('endDatetime')
+            ->add('startDatetime', null, array('empty_value' => $emptyValue))
+            ->add('endDatetime', null, array('empty_value' => $emptyValue))
             ->add('noTime', null, array('required' => false));
     }
 }
