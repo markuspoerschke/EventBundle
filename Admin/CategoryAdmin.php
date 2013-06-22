37d27d4cd38b2c2ac67f5e67b24a67a13b1492b3
Markus Poerschke <markus@eluceo.de>
2013-06-22 09:36:08 +0200

    Configure show fields

--- Admin/CategoryAdmin.php
+++ Admin/CategoryAdmin.php
@@ -5,6 +5,7 @@ namespace Eluceo\EventBundle\Admin;
 use Sonata\AdminBundle\Admin\Admin;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
 
 class CategoryAdmin extends Admin
 {
@@ -21,5 +22,13 @@ class CategoryAdmin extends Admin
         $list
             ->addIdentifier('name')
             ->add('active');
+    }
+
+    protected function configureShowFields(ShowMapper $show)
+    {
+        $show
+            ->add('name')
+            ->add('active')
+            ->add('uniqueSlug');
     }
 }
